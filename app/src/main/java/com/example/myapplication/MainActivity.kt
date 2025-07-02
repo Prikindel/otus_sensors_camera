15c05699d5d82d61155011d75b7b7379d4c4c5ff
KiryakovMaxim <kiryakovm96@gmail.com>
2025-07-02 22:08:51 +0300

    Show device sensors

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.example.myapplication
 
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var sensorManager: SensorManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showDeviceSensors()
+    }
+
+    private fun showDeviceSensors() {
+        val sensorManager = getSystemService(SensorManager::class.java)
+        val sensors = sensorManager.getSensorList(Sensor.TYPE_LIGHT)
+
+        Log.d("SENSORS", "List:  $sensors ")
     }
 
 }
