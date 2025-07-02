017a3bfbae91eee671d703824eec393add449282
KiryakovMaxim <kiryakovm96@gmail.com>
2025-07-02 22:48:51 +0300

    Take video from preview

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.activity.result.ActivityResultLauncher
@@ -14,6 +15,13 @@ import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.FileOutputOptions
+import androidx.camera.video.Quality
+import androidx.camera.video.QualitySelector
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
+import androidx.camera.video.VideoRecordEvent
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -32,6 +40,9 @@ class MainActivity : AppCompatActivity() {
         }
     private lateinit var imageCapture: ImageCapture
 
+    private lateinit var videoCapture: VideoCapture<Recorder>
+    private var currentRecording: Recording? = null
+
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
@@ -43,11 +54,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.takePhotoButton.setOnClickListener {
-            if (isTookPhoto) {
-                showPreview()
-            } else {
-                takePhoto()
+            when {
+                isTookPhoto -> showPreview()
+                currentRecording == null -> startRecording()
+                currentRecording != null -> stopRecording()
+                else -> takePhoto()
             }
+//            if (isTookPhoto) {
+//                showPreview()
+//            } else {
+//                takePhoto()
+//            }
 //            takePhotoLauncher.launch(null)
         }
 
@@ -69,8 +86,13 @@ class MainActivity : AppCompatActivity() {
             val preview = Preview.Builder()
                 .build()
 
-            imageCapture = ImageCapture.Builder()
+//            imageCapture = ImageCapture.Builder()
+//                .build()
+
+            val recorder = Recorder.Builder()
+                .setQualitySelector(QualitySelector.from(Quality.HD))
                 .build()
+            videoCapture = VideoCapture.withOutput(recorder)
 
             val cameraSelector = CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
@@ -78,54 +100,74 @@ class MainActivity : AppCompatActivity() {
 
             preview.setSurfaceProvider(findViewById<PreviewView>(R.id.image_preview).surfaceProvider)
 
-            camera.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+//            camera.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            camera.bindToLifecycle(this, cameraSelector, preview, videoCapture)
 
         }, ContextCompat.getMainExecutor(this))
     }
 
-    private fun takePhoto() {
+    private fun startRecording() {
         val outputDirectory = getOutputDirectory()
-        val photoFile = File(
+        val videoFile = File(
             outputDirectory,
             SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS", Locale.getDefault())
-                .format(System.currentTimeMillis()) + ".jpg"
+                .format(System.currentTimeMillis()) + ".mp4"
         )
 
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        val outputOptions = FileOutputOptions.Builder(videoFile).build()
 
-        imageCapture.takePicture(
-            outputOptions,
-            ContextCompat.getMainExecutor(this),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                    // Обработка сохраненного снимка
-                    // Здесь вы можете выполнить любые действия после сохранения изображения
-                    // Например, можно отобразить сообщение пользователю или открыть снимок в другом экране
-                    val savedUri = outputFileResults.savedUri
+        binding.takePhotoButton.text = getString(R.string.stop_recording)
 
-                    // Пример: запуск новой активности для отображения сохраненного изображения
-                    // val intent = Intent(this@MainActivity, DisplayImageActivity::class.java).apply {
-                    //     putExtra("image_uri", savedUri.toString())
-                    // }
-                    // startActivity(intent)
+        currentRecording = videoCapture.output
+            .prepareRecording(this, outputOptions)
+            .start(ContextCompat.getMainExecutor(this)) { recordEvent ->
+                when (recordEvent) {
+                    is VideoRecordEvent.Start -> {
+                        // Начало записи видео
+                        Log.d("CameraX", "Recording started")
+                    }
+                    is VideoRecordEvent.Finalize -> {
+                        // Завершение записи видео
+                        if (!recordEvent.hasError()) {
+                            Log.d("CameraX", "Video saved successfully: ${recordEvent.outputResults.outputUri}")
+                        } else {
+                            Log.e("CameraX", "Video recording error: ${recordEvent.error}")
+                        }
+                        currentRecording?.close()
+                        currentRecording = null
 
-                    savedUri?.let(::showImageUri)
-                }
-
-                override fun onError(exception: ImageCaptureException) {
-                    // Обработка ошибок
+                        showVideoUri(recordEvent.outputResults.outputUri)
+                    }
                 }
             }
-        )
+    }
+
+    private fun stopRecording() {
+        currentRecording?.stop()
     }
 
     private fun showPreview() {
         isTookPhoto = false
         with(binding) {
             image.visibility = View.GONE
+            videoView.visibility = View.GONE
             imagePreview.visibility = View.VISIBLE
 
             takePhotoButton.text = getString(R.string.take_photo)
+        }
+    }
+
+    private fun showVideoUri(uri: Uri) {
+        isTookPhoto = true
+        with(binding) {
+            videoView.setVideoURI(uri)
+
+            videoView.visibility = View.VISIBLE
+            imagePreview.visibility = View.GONE
+
+            takePhotoButton.text = getString(R.string.retake_photo)
+
+            videoView.start()
         }
     }
 
@@ -163,6 +205,42 @@ class MainActivity : AppCompatActivity() {
             File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
         }
         return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
+    }
+
+    private fun takePhoto() {
+        val outputDirectory = getOutputDirectory()
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS", Locale.getDefault())
+                .format(System.currentTimeMillis()) + ".jpg"
+        )
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    // Обработка сохраненного снимка
+                    // Здесь вы можете выполнить любые действия после сохранения изображения
+                    // Например, можно отобразить сообщение пользователю или открыть снимок в другом экране
+                    val savedUri = outputFileResults.savedUri
+
+                    // Пример: запуск новой активности для отображения сохраненного изображения
+                    // val intent = Intent(this@MainActivity, DisplayImageActivity::class.java).apply {
+                    //     putExtra("image_uri", savedUri.toString())
+                    // }
+                    // startActivity(intent)
+
+                    savedUri?.let(::showImageUri)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    // Обработка ошибок
+                }
+            }
+        )
     }
 
 }
