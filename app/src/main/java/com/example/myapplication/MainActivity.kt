ffcc69fe343d8499ec8881fd8d6e40ff4ce1dd37
KiryakovMaxim <kiryakovm96@gmail.com>
2025-07-02 22:38:26 +0300

    Take photo from preview

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,18 +2,25 @@ package com.example.myapplication
 
 import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.myapplication.databinding.ActivityMainBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 private const val REQUEST_PERMISSION = 1
 
@@ -23,9 +30,12 @@ class MainActivity : AppCompatActivity() {
         registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
             bitmap?.let(::showBitmap)
         }
+    private lateinit var imageCapture: ImageCapture
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
+    private var isTookPhoto = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +43,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.takePhotoButton.setOnClickListener {
+            if (isTookPhoto) {
+                showPreview()
+            } else {
+                takePhoto()
+            }
 //            takePhotoLauncher.launch(null)
         }
 
@@ -54,16 +69,64 @@ class MainActivity : AppCompatActivity() {
             val preview = Preview.Builder()
                 .build()
 
+            imageCapture = ImageCapture.Builder()
+                .build()
+
             val cameraSelector = CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build()
 
             preview.setSurfaceProvider(findViewById<PreviewView>(R.id.image_preview).surfaceProvider)
 
-            camera.bindToLifecycle(this, cameraSelector, preview)
-
+            camera.bindToLifecycle(this, cameraSelector, preview, imageCapture)
 
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto() {
+        val outputDirectory = getOutputDirectory()
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS", Locale.getDefault())
+                .format(System.currentTimeMillis()) + ".jpg"
+        )
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    // Обработка сохраненного снимка
+                    // Здесь вы можете выполнить любые действия после сохранения изображения
+                    // Например, можно отобразить сообщение пользователю или открыть снимок в другом экране
+                    val savedUri = outputFileResults.savedUri
+
+                    // Пример: запуск новой активности для отображения сохраненного изображения
+                    // val intent = Intent(this@MainActivity, DisplayImageActivity::class.java).apply {
+                    //     putExtra("image_uri", savedUri.toString())
+                    // }
+                    // startActivity(intent)
+
+                    savedUri?.let(::showImageUri)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    // Обработка ошибок
+                }
+            }
+        )
+    }
+
+    private fun showPreview() {
+        isTookPhoto = false
+        with(binding) {
+            image.visibility = View.GONE
+            imagePreview.visibility = View.VISIBLE
+
+            takePhotoButton.text = getString(R.string.take_photo)
+        }
     }
 
     private fun requestPermission() {
@@ -78,9 +141,28 @@ class MainActivity : AppCompatActivity() {
         binding.image.setImageBitmap(bitmap)
     }
 
+    private fun showImageUri(uri: Uri) {
+        isTookPhoto = true
+        with(binding) {
+            image.setImageURI(uri)
+
+            image.visibility = View.VISIBLE
+            imagePreview.visibility = View.GONE
+
+            takePhotoButton.text = getString(R.string.retake_photo)
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
     }
 
 }
