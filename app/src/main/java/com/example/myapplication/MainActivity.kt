2c7702c00e164e570b8009bf1a6555cb15630d6a
KiryakovMaxim <kiryakovm96@gmail.com>
2025-07-02 22:20:23 +0300

    Take photo from system camera

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,40 @@
 package com.example.myapplication
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val takePhotoLauncher: ActivityResultLauncher<*> =
+        registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
+            bitmap?.let(::showBitmap)
+        }
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.takePhotoButton.setOnClickListener {
+            takePhotoLauncher.launch(null)
+        }
+    }
+
+    private fun showBitmap(bitmap: Bitmap) {
+        binding.image.setImageBitmap(bitmap)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
